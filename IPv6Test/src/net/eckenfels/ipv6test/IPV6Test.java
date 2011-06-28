d2d6b75b2da53e97b70049fb34ce9ed4bcc2244d
Bernd Eckenfels <bernd-2011@eckenfes.net>
2011-06-28 18:23:56 +0200

    Initial Source

---
+++ IPv6Test/src/net/eckenfels/ipv6test/IPV6Test.java
@@ -0,0 +1,26 @@
+/*
+ * IPV6Test.java
+ *
+ * created at 2011-06-28by Eckenfel <YOURMAILADDRESS>
+ * 
+ * $Id: $
+ *
+ * Copyright (c) 2011 SEEBURGER AG, Germany. All Rights Reserved.
+ */
+package net.eckenfels.ipv6test;
+
+
+public class IPV6Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
